ff2cb8f8fb47f8293eb3483d2cdf5e2edc1e301b
Ian Rose <ian.r.rose@gmail.com>
2015-07-06 18:47:03 -0700

    Add figure script

---
+++ figures/stability_region.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('ian')
+plt.figure(figsize=(8,4) )
+
+x_nsfd = np.linspace(1.e-10,5, 1000.)
+y_nsfd = np.piecewise( x_nsfd , [x_nsfd <= 2., x_nsfd > 2.], [lambda x : 1.e6, lambda x : -x*np.log(1.-2./x)] )
+x_kmm = np.linspace(1.e-10,1.-1.e-10, 1000.)
+y_kmm = 2./(1.-x_kmm)
+y_fe = np.ones_like(x_nsfd)*2.
+
+plt.subplot(121)
+plt.xlim(0.,5.)
+plt.ylim(0.,10.)
+plt.xlabel(r'$\tau^*/\tau_{\mathrm{max}}$')
+plt.ylabel(r'$\frac{\Delta t}{\tau_{\mathrm{max}}}$', rotation=0)
+plt.fill_between(x_nsfd, 0, y_nsfd, color='red', alpha=0.2)
+plt.fill_between(x_nsfd, 0, y_fe, color='blue', alpha=0.2)
+plt.subplot(122)
+plt.xlim(0.,1.)
+plt.ylim(0.,10.)
+plt.xlabel(r'\theta')
+plt.fill_between(x_kmm, 0, y_kmm, color='green', alpha=0.2)
+plt.fill_between(x_nsfd, 0, y_fe, color='blue', alpha=0.2)
+#plt.show()
+plt.savefig('stability.pdf')
