bddc87a07d01ecd864fc8580856f6a27c31bd40a
Ian Rose <ian.r.rose@gmail.com>
2016-05-24 11:59:11 -0700

    Fix error in identification of fundamental mode.

--- figures/benchmark_setup.py
+++ figures/benchmark_setup.py
@@ -10,7 +10,7 @@ ax.tick_params( top='off', bottom='off', left='off', right='off')
 
 amplitude = 0.1
 x = np.linspace(0,1,1000)
-surface = 1.0 + amplitude*np.cos(2.*np.pi*x)
+surface = 1.0 + amplitude*np.cos(np.pi*x)
 ref_surface = 1.0*np.ones_like(x)
 
 #plot fluid
@@ -22,8 +22,8 @@ plt.fill_between(x, 0, surface, color='gray', alpha=0.2)
 head_size = 0.02
 
 #Draw arrow for surface perturbation
-ax.arrow(0.5, 1.0, 0.0, -amplitude+head_size, head_width=head_size, head_length=head_size, fc='k', ec='k')
-ax.text(0.45, .95, r'$\zeta_0$', fontsize=20)
+ax.arrow(0.95, 1.0, 0.0, -0.8*amplitude+head_size, head_width=head_size, head_length=head_size, fc='k', ec='k')
+ax.text(0.9, .95, r'$\zeta_0$', fontsize=20)
 
 #Draw arrow for depth
 ax.arrow(0.05, 0.05, 0.0, 0.9, head_width=head_size, head_length=head_size, fc='k', ec='k')
